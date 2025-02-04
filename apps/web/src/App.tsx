07546dc27cc671e6abfc3a65bd2058f17aaf00df
Richard Foster <richard@rpf.me>
2025-02-04 00:32:32 +0000

    Styling tweaks

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -54,29 +54,29 @@ function HomePage() {
   }, [feeds, refresh]);
 
   return (
-    <main className="flex flex-row">
+    <main className="flex flex-row gap-4">
       <header className="flex flex-col gap-4 p-4">
         <div className="text-3xl">✌️</div>
         <section>
           <span className="font-bold">add a feed</span>
-          <form onSubmit={submit} className="flex flex-row items-center gap-2">
+          <form onSubmit={submit} className="flex flex-col items-center gap-2">
             <input
               type="text"
-              className="bg-white border border-black p-1 px-2"
+              className="bg-white border-2 rounded-sm border-foreground p-1 px-2"
               placeholder="Feed URL"
               value={url}
               onChange={(e) => setUrl(e.currentTarget.value)}
             />
-            <button className="bg-black p-1 px-2 text-white">add</button>
-            <div className="text-red-500">
+            <button className="v-button w-full">Add</button>
+            <div className="text-red-500 text-sm">
               {addFeed.error && "Something bad went wrong"}
             </div>
           </form>
         </section>
         <section>
-          <span className="font-bold">refresh feeds</span>
+          <span className="block font-bold">refresh feeds</span>
           <button
-            className="block bg-black p-1 px-2 text-white"
+            className="v-button"
             disabled={refresh.isLoading}
             onClick={() => reload()}
           >
@@ -85,7 +85,7 @@ function HomePage() {
         </section>
       </header>
 
-      <article className="p-4 pt-2">
+      <article className="p-4">
         <span className="block font-bold pb-4">
           latest {feeds.isLoading && "(loading...)"}
         </span>
@@ -143,7 +143,7 @@ function Feed({ feed }: { feed: FeedWithItems }) {
 
       {items.hasNextPage && (
         <button
-          className="cursor-pointer bg-gray-50 text-sm pb-2 px-2 rounded-sm"
+          className="cursor-pointer bg-white/50 text-foreground text-sm pb-2 px-2 rounded-sm"
           onClick={() => items.fetchNextPage()}
         >
           &hellip;
@@ -166,10 +166,22 @@ function TimeBadge({ time }: { time?: number | null | string }) {
     <span
       className={classNames(
         "px-2 py-1 rounded-sm text-xs scale-75",
-        Date.now() - timeF < WEEK ? "bg-green-600 text-white" : " bg-gray-200"
+        Date.now() - timeF < WEEK ? "bg-green-600 text-white" : " bg-white/70"
       )}
     >
       <TimeAgo time={timeF} />
     </span>
   );
 }
+
+// function Button(props: ComponentProps<"button">) {
+//   return (
+//     <button
+//       {...props}
+//       className={classNames(
+//         "cursor-pointer bg-foreground p-1 px-2 text-background rounded-sm",
+//         props.className
+//       )}
+//     />
+//   );
+// }
