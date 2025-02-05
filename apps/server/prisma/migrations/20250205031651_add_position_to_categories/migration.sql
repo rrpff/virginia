58bdaed83f5d5e131b1a94db3e6bbe4a6ca3214e
Richard Foster <richard@rpf.me>
2025-02-05 03:17:35 +0000

    Add position to category entities

---
+++ apps/server/prisma/migrations/20250205031651_add_position_to_categories/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "vanity" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "position" INTEGER NOT NULL DEFAULT 0
+);
+INSERT INTO "new_Category" ("icon", "id", "name", "vanity") SELECT "icon", "id", "name", "vanity" FROM "Category";
+DROP TABLE "Category";
+ALTER TABLE "new_Category" RENAME TO "Category";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
