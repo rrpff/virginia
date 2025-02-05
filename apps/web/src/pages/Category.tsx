3b3e5ed045219d63ad7670938f7ae23b85e8d8a6
Richard Foster <richard@rpf.me>
2025-02-05 06:20:36 +0000

    More space

--- apps/web/src/pages/Category.tsx
+++ apps/web/src/pages/Category.tsx
@@ -13,7 +13,7 @@ export default function CategoryPage() {
 
   return (
     <article>
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-15">
         {feeds.data?.map((feed) => (
           <Feed key={feed.url} feed={feed} items={feed.items} />
         ))}
