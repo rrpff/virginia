1e2b9ee795da502fb041a805d8ce6dfd81fe4379
Richard Foster <richard@rpf.me>
2025-02-05 22:44:41 +0000

    Use Combobox

--- apps/web/src/components/FeedDefinitionInput.tsx
+++ apps/web/src/components/FeedDefinitionInput.tsx
@@ -1,47 +1,63 @@
-import { ComponentProps, useState } from "react";
+import { useState } from "react";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxInputProps,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
 import { rpc, RpcOutputs } from "../rpc";
+import classNames from "classnames";
 
 type FeedDefinition = RpcOutputs["feedDefinitions"][number];
 
-type Props = ComponentProps<"input"> & {
+type Props = ComboboxInputProps & {
   onSelectFeed?: (result: FeedDefinition) => void;
 };
 
 export function FeedDefinitionInput({ onSelectFeed, ...props }: Props) {
-  const [url, setUrl] = useState("");
-  const query = rpc.feedDefinitions.useQuery({ url }, { enabled: isURL(url) });
+  const [query, setQuery] = useState("");
+
+  const results = rpc.feedDefinitions.useQuery(
+    { url: query },
+    { enabled: isURL(query) }
+  );
 
   return (
-    <div>
-      <input
-        type="text"
-        className="v-input w-full max-w-96"
-        value={url}
-        onChange={(e) => setUrl(e.currentTarget.value)}
+    <Combobox
+      onChange={(result: FeedDefinition) => {
+        if (result !== null) {
+          onSelectFeed?.(result);
+        }
+      }}
+    >
+      <ComboboxInput
         {...props}
+        className={classNames(
+          "v-input w-96 data-[open]:rounded-b-none!",
+          props.className
+        )}
+        onChange={(e) => setQuery(e.currentTarget.value)}
       />
-      {/* TODO: replace with actual autocomplete */}
-      {query.data?.length && (
-        <div className="p-4 bg-white max-w-96 flex flex-col gap-2">
-          {query.data?.map((result) => (
-            <span
-              key={result.url}
-              className="flex flex-row gap-2"
-              onClick={() => {
-                setUrl("");
-                onSelectFeed?.(result);
-              }}
-            >
-              <img src={result.iconUrl ?? ""} className="v-icon" />
-              <div className="flex flex-col">
-                <span className="font-bold">{result.name}</span>
-                <span className="text-sm line-clamp-1">{result.url}</span>
-              </div>
-            </span>
-          ))}
-        </div>
-      )}
-    </div>
+      <ComboboxOptions
+        anchor="bottom start"
+        className="bg-white w-96 border-2 border-t-0 border-foreground rounded-b-sm empty:invisible"
+      >
+        {results.data?.map((result) => (
+          <ComboboxOption
+            key={result.url}
+            value={result}
+            className="flex flex-row gap-2 cursor-pointer p-2 data-[focus]:bg-foreground data-[focus]:text-background"
+          >
+            <img className="v-icon" src={result.iconUrl ?? ""} />
+            <div className="flex flex-col">
+              <span className="font-bold">{result.name}</span>
+              <span className="text-sm line-clamp-1">{result.url}</span>
+            </div>
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
   );
 }
 
