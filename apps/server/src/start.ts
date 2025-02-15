78f5584f3402b90f400eb0d441fb64580aed6ef4
Richard Foster <richard@rpf.me>
2025-02-15 18:09:00 +0000

    Give default prod configuration to server

--- apps/server/src/start.ts
+++ apps/server/src/start.ts
@@ -4,10 +4,13 @@ import { start } from "./index.js";
   const server = await start({
     host: process.env.HOST ?? "0.0.0.0",
     port: Number(process.env.PORT ?? 26541),
-    migrateOnStart: process.env.NODE_ENV === "production",
-    refreshOnStart: process.env.NODE_ENV === "production",
-    serveFrontend: process.env.NODE_ENV === "production",
-    publicRoot: process.env.PUBLIC_ROOT || undefined,
+    migrateOnStart: process.env.NODE_ENV !== "development",
+    refreshOnStart: process.env.NODE_ENV !== "development",
+    serveFrontend: process.env.NODE_ENV !== "development",
+    publicRoot:
+      process.env.NODE_ENV !== "development"
+        ? "/snapshot/virginia/apps/web/dist" // location within pkg binary
+        : undefined,
   });
 
   server.log.on("info", (...args) => console.info(...args));
